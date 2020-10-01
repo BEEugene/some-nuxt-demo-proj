9fd56b25c76d1368bd095f1e195ebd294d040799
beeugene <evgenii.baraboshkin@skoltech.ru>
2020-10-01 15:06:25 +0300

    Initial commit

---
+++ pages/test/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="my-strictly-unique-vue-upload-multiple-image" style="display: flex; justify-content: center;">
+    <vue-upload-multiple-image
+      @upload-success="uploadImageSuccess"
+      @before-remove="beforeRemove"
+      @edit-image="editImage"
+      :data-images="images"
+      idUpload="myIdUpload"
+      editUpload="myIdEdit"
+      dragText = "Drag an image here"
+      browseText = "(or click here to browse)"
+      primaryText = "Default Image"
+      markIsPrimaryText = "Set as default image"
+      popupText = "This image will be set as default"
+      dropText = "Drag and drop"
+      accept = image/jpeg,image/png,image/jpg,image/tif,image/tiff
+    ></vue-upload-multiple-image>
+  </div>
+</template>
+
+<script>
+import VueUploadMultipleImage from 'vue-upload-multiple-image'
+export default {
+  name: "AppUpload",
+  components:{
+    VueUploadMultipleImage},
+  data(){
+    return{
+      file:"",
+      images: []
+    }
+  },
+  methods:{
+
+    uploadImageSuccess(formData, index, fileList) {    },
+    beforeRemove (index, done, fileList) {    },
+    editImage (formData, index, fileList) {   },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
